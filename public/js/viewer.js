4030d71374d1d3e8827f21fb57c4dbed034c96ee
Sunny Ripert <sunny@sunfox.org>
2019-09-18 16:31:10 +0200

    Use higher definition

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -25,7 +25,7 @@ viewer.setParameter('InitRotationZ', options.z)
 viewer.setParameter('RenderMode', 'flat') // can be 'smooth'
 viewer.setParameter('Renderer', 'webgl')
 viewer.setParameter('Background', 'off')
-viewer.setParameter('Definition', 'standard')
+viewer.setParameter('Definition', 'high')
 viewer.setParameter('ProgressBar', 'off')
 viewer.onloadingcomplete = function() {
   window._loadingComplete = true
